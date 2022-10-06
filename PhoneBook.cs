15058648b98a4b737404be39aa3926ab3ff5c574
Avezz78 <113528051+Avezz78@users.noreply.github.com>
2022-10-06 19:06:37 +0530

    Add files via upload

---
+++ PhoneBook.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneBook
+{
+    internal class PhoneBook // this class is responsible for managing contact list
+    {
+        private List<Contact> contacts { get; set; } = new List<Contact>();//this is private field bcz we dont want anyone outside the class to modify the contact list
+
+        private void DisplayContactDetails(Contact contact) //creating p.method to display contacts
+        {
+            Console.WriteLine($"Contact: {contact.Name},{contact.Number}");
+        }
+
+        private void DisplayContactDetails(List<Contact> contacts)
+        {
+            foreach (var contact in contacts)
+            {
+                DisplayContactDetails(contact);
+            }
+        }
+        public void AddContact(Contact contact) // this method is to Add contacts to contactlist ,taking parameter as  contact object
+        {
+            contacts.Add(contact);
+        }
+
+        public void DisplayContact(string number) // this method is to display a contact by numb,tat will accept string num as parameter
+        {
+            var contact = contacts.FirstOrDefault(c => c.Number == number); //will b able to get the merged contact based on lambda expression
+                                                                            //x => x.Number == number - lambda exp by doing tat we either get null value if contact is not found based on numb or some speific contact
+
+            if (contact == null)
+            {
+                Console.WriteLine("contact not found");
+            }
+            else
+            {
+                DisplayContactDetails(contact);
+            }
+        }
+
+        // to display all contacts from phonebook so creating one more method DisplayAllContact()
+
+        public void DisplayAllContact()
+        {
+            DisplayContactDetails(contacts);
+        }
+
+        public void DisplayMatchingContacts(string searchphraase)
+        {
+            // named:sahil
+            //searchphrase:sa 
+            var matchingContacts = contacts.Where(c => c.Name.Contains(searchphraase)).ToList();
+            DisplayContactDetails(matchingContacts);
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneBook
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // for operations taking user inputs
+            Console.WriteLine("hello from the consele phoneBook app");
+            Console.WriteLine("select operationn");
+            Console.WriteLine("1 add contact");
+            Console.WriteLine("2 Display contact by number");
+            Console.WriteLine("3 view all contacts");
+            Console.WriteLine("4 search for contacts for a given name");
+            Console.WriteLine("press x to exit");
+
+            var userInput = Console.ReadLine(); //to get user inputs 
+            var PhoneBook = new PhoneBook();    // creating object of phonebook
+
+            while (true) // in order to not terminate when user selects 1st operation will have to create a while loop until user explicitly wants to exit
+            {
+                switch (userInput)
+                {
+                    case "1": // for adding new contacts 
+                        Console.WriteLine("contact name: "); //for user info
+                        var name = Console.ReadLine(); 
+                        Console.WriteLine("contact number:");
+                        var number = Console.ReadLine();
+
+                        var newContact = new Contact(name, number); //creating object of new contact n passing name n num as parameters
+                        PhoneBook.AddContact(newContact);
+                        break;
+
+                    case "2":
+                        Console.WriteLine("contact num to search");
+                        var SearchNumber = Console.ReadLine();
+                        PhoneBook.DisplayContact(SearchNumber);
+                        break;
+
+                    case "3": // to display all contact
+                        PhoneBook.DisplayAllContact();
+                        break;
+
+                    case "4": // to search for a contact for a given name
+                        Console.WriteLine("Name search phrase");
+                        var searchPhrase= Console.ReadLine();   // to get value from user
+                        PhoneBook.DisplayMatchingContacts(searchPhrase);
+                        break;
+                    case "x": // this case is for user to knw how to exit
+                        return;
+
+                    default: // when user enters wrong option
+                        Console.WriteLine("select valid option");
+                        break;
+                }
+                Console.WriteLine("select option");
+                userInput = Console.ReadLine(); 
+
+            }
+            }
+        }
+    }
